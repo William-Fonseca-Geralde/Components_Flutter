baff965176d89129353f2dfa3748033d78255668
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2025-02-26 15:58:50 -0300

    adicionado botões de navegação do Drawer de explicação para facilitar em tela menores

--- components_flutter/lib/src/views/widgets/end_drawers/end_drawer_widget.dart
+++ components_flutter/lib/src/views/widgets/end_drawers/end_drawer_widget.dart
@@ -7,60 +7,97 @@ class EndDrawerWidget extends ConsumerStatefulWidget {
   const EndDrawerWidget({super.key, required this.selectedComponent});
 
   final String selectedComponent;
-
   @override
   ConsumerState<EndDrawerWidget> createState() => _EndDrawerWidgetState();
 }
 
-class _EndDrawerWidgetState extends ConsumerState<EndDrawerWidget> {
+class _EndDrawerWidgetState extends ConsumerState<EndDrawerWidget>
+    with SingleTickerProviderStateMixin {
   bool isExpanded = false;
+  late TabController tabController;
+
+  late Map<String, dynamic> component;
+  late final Map<String, dynamic> components;
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
     final buttonsComponents = componentTips["Actions"]["CommonButtons"];
     final fabsComponents = componentTips["Actions"]["FloatingButtons"];
 
-    final Map components = {
+    components = {
       "Botões Comuns": buttonsComponents,
       "Botões de Ação Flutuante": fabsComponents,
     };
 
-    final component =
-        (components[widget.selectedComponent] as Map<String, dynamic>);
+    _updateComponent();
 
+    tabController = TabController(length: component.length, vsync: this);
+    tabController.addListener(handleTabChange);
+  }
+
+  void _updateComponent() {
+    component = (components[widget.selectedComponent] as Map<String, dynamic>);
+  }
+
+  @override
+  void dispose() {
+    tabController.removeListener(handleTabChange);
+    tabController.dispose();
+    super.dispose();
+  }
+
+  void handleTabChange() {
+    setState(() {});
+  }
+
+  void nextTab() {
+    if (tabController.index < tabController.length - 1) {
+      tabController.animateTo(tabController.index + 1);
+    }
+  }
+
+  void previousTab() {
+    if (tabController.index > 0) {
+      tabController.animateTo(tabController.index - 1);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
         double drawerWidth = constraints.maxWidth * 0.75;
         if (constraints.maxWidth > 850) {
-          drawerWidth = constraints.maxWidth * 0.55;
+          drawerWidth = constraints.maxWidth * 0.65;
         }
 
         return Drawer(
           width: drawerWidth,
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                height: 125,
-                child: DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primaryContainer,
-                  ),
-                  child: Text(
-                    widget.selectedComponent != '' &&
-                            components.containsKey(widget.selectedComponent)
-                        ? widget.selectedComponent
-                        : "Componente não selecionado",
-                    style: Theme.of(context).textTheme.headlineLarge,
+          child: Scaffold(
+            body: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 125,
+                  child: DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                    ),
+                    child: Text(
+                      widget.selectedComponent != '' &&
+                              components.containsKey(widget.selectedComponent)
+                          ? widget.selectedComponent
+                          : "Componente não selecionado",
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
                   ),
                 ),
-              ),
-              DefaultTabController(
-                length: component.length,
-                child: Column(
+                Column(
                   children: [
                     TabBar(
+                      controller: tabController,
                       isScrollable: true,
                       labelColor: Theme.of(context).colorScheme.primary,
                       unselectedLabelColor: Colors.grey,
@@ -72,6 +109,7 @@ class _EndDrawerWidgetState extends ConsumerState<EndDrawerWidget> {
                     SizedBox(
                       height: constraints.maxHeight * 0.74,
                       child: TabBarView(
+                        controller: tabController,
                         children:
                             component.values.map((comp) {
                               return _buildComponentDetails(
@@ -84,8 +122,36 @@ class _EndDrawerWidgetState extends ConsumerState<EndDrawerWidget> {
                     ),
                   ],
                 ),
-              ),
-            ],
+              ],
+            ),
+            floatingActionButton:
+                MediaQuery.sizeOf(context).width < 800
+                    ? Stack(
+                      children: [
+                        if (tabController.index > 0)
+                          Positioned(
+                            left: 25,
+                            bottom: 16,
+                            child: FloatingActionButton(
+                              heroTag: "previousBtn",
+                              onPressed: previousTab,
+                              child: Icon(Icons.arrow_back),
+                            ),
+                          ),
+
+                        if (tabController.index < tabController.length - 1)
+                          Positioned(
+                            right: 25,
+                            bottom: 16,
+                            child: FloatingActionButton(
+                              heroTag: "nextBtn",
+                              onPressed: nextTab,
+                              child: Icon(Icons.arrow_forward),
+                            ),
+                          ),
+                      ],
+                    )
+                    : null,
           ),
         );
       },
@@ -108,9 +174,9 @@ class _EndDrawerWidgetState extends ConsumerState<EndDrawerWidget> {
             LayoutBuilder(
               builder: (context, constraints) {
                 return Wrap(
-                  runSpacing: 45,
+                  runSpacing: 30,
                   alignment: WrapAlignment.center,
-                  spacing: 45,
+                  spacing: 30,
                   children: [
                     Column(
                       spacing: 15,
@@ -120,7 +186,7 @@ class _EndDrawerWidgetState extends ConsumerState<EndDrawerWidget> {
                             padding: const EdgeInsets.all(8),
                             child: SizedBox(
                               width:
-                                  constraints.maxWidth < 700
+                                  constraints.maxWidth < 800
                                       ? constraints.maxWidth * 1.75
                                       : constraints.maxWidth * 0.45,
                               child: Card.outlined(
@@ -244,7 +310,7 @@ class _EndDrawerWidgetState extends ConsumerState<EndDrawerWidget> {
                                     in componentData["code"]?.entries ?? [])
                                   SizedBox(
                                     width:
-                                        constraints.maxWidth < 700
+                                        constraints.maxWidth < 800
                                             ? constraints.maxWidth * 1.75
                                             : constraints.maxWidth * 0.45,
                                     child: Card.outlined(
